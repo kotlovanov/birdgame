f0b65d3ef4c6a1d18a330653f9690be8f4fd72a2
kotlovanov <kotlovanovna@gmail.com>
2024-04-06 17:23:27 +0500

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.screens.ScreenGame;
+import com.mygdx.game.screens.ScreenRestart;
+
+public class MyGdxGame extends Game {
+
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
+
+    public static final int SCR_WIDTH = 1280;
+    public static final int SCR_HEIGHT = 720;
+
+    public ScreenGame screenGame;
+    public ScreenRestart screenRestart;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+
+        screenGame = new ScreenGame(this);
+        screenRestart = new ScreenRestart(this);
+        setScreen(screenGame);
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
